dd5648a1a2d8d560eaae88ec47deaaf08409b813
riera90 <diegorodriguezriera@gmail.com>
2018-08-20 02:25:21 +0200

    fixed user folder creation

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo mv ./generic-cli /usr/bin/generic-cli
 sudo chmod +x /usr/bin/generic-cli
-rm -rf ./../generic-CLI
 mkdir ~/.gcli
-rm ~/.gcli/gcli_banner.txt
+mv ./gcli_banner.txt ~/.gcli/gcli_banner.txt
+rm -rf ./../generic-CLI
