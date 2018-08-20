5224190d2c13c7a827cbdaa50f9930c919ee5d5b
riera90 <diegorodriguezriera@gmail.com>
2018-08-20 02:16:16 +0200

    moved gcli to .gcli

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 sudo mv ./generic-cli /usr/bin/generic-cli
 sudo chmod +x /usr/bin/generic-cli
 rm -rf ./../generic-CLI
-mkdir ~/gcli
-rm ~/gcli/gcli_banner.txt
+mkdir ~/.gcli
+rm ~/.gcli/gcli_banner.txt
